aff1403d49632639a7cbeddb0fa177939f796201
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2022-02-09 14:43:57 -0500

    instalacion de Sequelize ORM

--- services/product.service.js
+++ services/product.service.js
@@ -1,14 +1,13 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
 
   constructor(){
     this.products = [];
     this.generate();
-    this.pool= pool;
-    this.pool.on('error', (err) => console.error(err));
+
   }
 
   generate() {
@@ -35,8 +34,8 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM task';
-    const respuesta = await this.pool.query(query);
-    return respuesta.rows;
+    const [data, metadata] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
