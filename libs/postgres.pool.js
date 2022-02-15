165e7cabcd9902ad452b227c224175206bd6d832
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2022-02-15 17:25:01 -0500

    configuracion de heroku

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -2,10 +2,14 @@ const { Pool } = require('pg');
 const { config } = require('../config/config');
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-console.log(URI);
+let URI = '';
 
-
-  const pool = new Pool({ connectionString : URI});
+if (config.isProd){
+  URI = config.dbUrl;
+}
+else{
+  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
+const pool = new Pool({ connectionString : URI});
 
 module.exports = pool;
